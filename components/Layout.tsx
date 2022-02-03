7309190e64f9e5b28eea1e68fd3564235bcd2e52
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-02-03 21:12:43 +0700

    feat(layout): add initial layout: header, main, footer

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+export default function Layout({ children }: { children: JSX.Element }) {
+  return (
+    <div>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
-  return <div>Lorem, ipsum.</div>;
+  return (
+    <Layout>
+      <div>Main Content</div>
+    </Layout>
+  );
 };
 
 export default Home;
