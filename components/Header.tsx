3636193532a6b7a6ab31b2ad8b03c52419263895
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-02-04 01:27:07 +0700

    feat(header): add wireframe of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,14 @@
 export default function Header() {
-  return <header className="h-20 w-full bg-indigo-400 p-4">Header</header>;
+  return (
+    <header className="bg-gray-500 p-4">
+      <nav className="mx-auto flex max-w-2xl justify-between bg-gray-400">
+        <h3 className="bg-gray-300">LOGO</h3>
+        <ul className="flex space-x-4">
+          <li className="bg-gray-300">LOREM</li>
+          <li className="bg-gray-300">LOREM</li>
+          <li className="bg-gray-300">LOREM</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
