1e7586b6481af9c7ce4bfe04b3810ca3daf6949b
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-02-05 01:01:12 +0700

    feat(Hamburger): add classname support

--- components/Hamburger/Hamburger.tsx
+++ components/Hamburger/Hamburger.tsx
@@ -1,10 +1,14 @@
 export interface Props {
   backgroundColor?: "bg-gray-200" | "bg-indigo-400";
+  className?: string;
 }
 
-export default function Hamburger({ backgroundColor = "bg-gray-200" }: Props) {
+export default function Hamburger({
+  backgroundColor = "bg-gray-200",
+  className,
+}: Props) {
   return (
-    <button className="flex h-5 flex-col justify-between">
+    <button className={`flex h-5 flex-col justify-between ${className}`}>
       {[...Array(3)].map(() => (
         <div className={`h-[0.15rem] w-7 ${backgroundColor}`}></div>
       ))}
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,14 +6,14 @@ export interface Props {
 
 export default function Header({ backgroundColor = "bg-primary" }: Props) {
   return (
-    <header className={`p-4 ${backgroundColor}`}>
-      <nav className="mx-auto flex h-6 max-w-2xl items-center justify-between bg-gray-300/50">
-        <h3 className="bg-gray-300/50">LOGO</h3>
-        <Hamburger />
-        <ul className="hidden space-x-4">
-          <li className="bg-gray-300/50">LOREM</li>
-          <li className="bg-gray-300/50">LOREM</li>
-          <li className="bg-gray-300/50">LOREM</li>
+    <header className={`px-4 py-2 ${backgroundColor}`}>
+      <nav className="mx-auto flex h-8 max-w-2xl items-center justify-between text-lg font-semibold text-gray-200">
+        <h3 className="">LOGO</h3>
+        <Hamburger className="sm:hidden" />
+        <ul className="hidden space-x-4 sm:flex">
+          <li className="">LOREM</li>
+          <li className="">LOREM</li>
+          <li className="">LOREM</li>
         </ul>
       </nav>
     </header>
