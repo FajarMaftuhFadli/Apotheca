1ceb2bf66397e82411ef350c2c17ed1f19d36f45
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-02-04 11:05:12 +0700

    feat(design): add header preset

--- pages/design/index.tsx
+++ pages/design/index.tsx
@@ -1,9 +1,24 @@
 export default function index() {
   return (
-    <div className="w-full p-4 py-10">
-      <div className="mx-auto flex w-full max-w-2xl justify-evenly">
-        <Card color="bg-indigo-400" />
+    <div className="mx-auto flex w-full max-w-3xl flex-col space-y-10 p-4 py-10">
+      <div className="flex w-full flex-wrap gap-8">
+        <Card color="bg-sky-300" />
         <Card />
+        <Card color="bg-sky-400" />
+        <Card />
+      </div>
+      <div className="flex flex-col space-y-4 rounded-md bg-gray-100 p-4 text-center ring-2 ring-slate-300">
+        <h1 className="text-6xl">Lorem ipsum dolor sit amet.</h1>
+        <h2 className="text-5xl">Lorem ipsum dolor sit amet.</h2>
+        <h3 className="text-4xl">Lorem ipsum dolor sit amet.</h3>
+        <h4 className="text-3xl">Lorem ipsum dolor sit amet.</h4>
+        <h5 className="text-2xl">Lorem ipsum dolor sit amet.</h5>
+        <h6 className="text-xl">Lorem ipsum dolor sit amet.</h6>
+      </div>
+      <div>
+        <button className="rounded-md bg-indigo-400 p-2 font-semibold tracking-wide text-gray-100 shadow-md">
+          BUTTON
+        </button>
       </div>
     </div>
   );
