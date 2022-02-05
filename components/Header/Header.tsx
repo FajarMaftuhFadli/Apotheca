64a47027ead9a8540b845e6cb842140d0040ac0d
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-02-05 14:22:26 +0700

    feat(Header): add LogoFull

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Hamburger from "../Hamburger/Hamburger";
+import LogoFull from "../Logo/LogoFull";
 
 export interface Props {
   backgroundColor?: "bg-primary" | "bg-secondary";
@@ -7,8 +8,10 @@ export interface Props {
 export default function Header({ backgroundColor = "bg-primary" }: Props) {
   return (
     <header className={`px-4 py-2 ${backgroundColor}`}>
-      <nav className="mx-auto flex h-8 max-w-2xl items-center justify-between text-lg font-semibold text-gray-200">
-        <h3 className="">LOGO</h3>
+      <nav className="mx-auto flex h-12 max-w-2xl items-center justify-between text-lg font-semibold text-gray-200">
+        <div className="h-full">
+          <LogoFull className="h-full" />
+        </div>
         <Hamburger className="sm:hidden" />
         <ul className="hidden space-x-4 sm:flex">
           {["MENU 1", "MENU 2", "MENU 3"].map((v, i) => (
